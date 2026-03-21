bf3377d6b9f7c0e01628876e73ca42939b2f0a13
ShengzhiWu <82275678+ShengzhiWu@users.noreply.github.com>
2026-03-22 02:14:18 +0800

    doc: new examples

---
+++ src/examples/binary-counting.js
@@ -0,0 +1,319 @@
+// 样例代码：二进制除法
+
+if (!examples)
+    var examples = {};
+
+examples["binary-counting"] = {};
+
+examples["binary-counting"]["name"] = {
+    "zh": "二进制计数",
+    "en": "Binary counting"
+}
+;
+examples["binary-counting"]["description"] = {
+    "zh": "这个图灵机实现了二进制计数。一开始纸带上有连续的一段B，位于机头右侧。结束时会在这段B右侧隔一格写上B的数量（二进制）。",
+    "en": "This Turing machine implements binary counting. Initially there is a contiguous segment of Bs on the tape, located to the right of the head. At the end, the machine will write the count of B in binary to the right of this segment of B, separated by a blank."
+};
+
+examples["binary-counting"]["category"] = {
+    "zh": "二进制运算/",
+    "en": "Binary Operations/"
+};
+
+examples["binary-counting"]["code"] = {};
+
+examples["binary-counting"]["code"]["zh"] = `// 二进制计数
+
+start, "", N, R
+start, other, N, N, 0
+
+0, "B", "B'", R, 1
+0, "", N, L, 5  // 计算已经完成，接下来擦掉特殊记号
+0, other, N, R
+
+1, "", N, R, 2
+1, other, N, R
+
+2, "", "1", L, 3
+2, "0", "1", L, 3
+2, "1", "0", R  // 进位
+
+3, "", N, L, 4
+3, other, N, L
+
+4, "", N, R, 0
+4, other, N, L
+
+5, "B'", "B", L
+5, "", N, R, 6  // 已经擦除完特殊记号，接下来反转结果。需要反转是因为先前为了计算方便，二进制数左边是低位
+
+6, "0", N, N, 7
+6, "1", N, N, 7
+6, other, N, R
+
+7, "0", "", R, 8
+7, "1", "", R, 9
+7, "", N, L, 15  // 计算逆转二进制字符串已经完成，接下来擦掉特殊记号
+7, other, N, R
+
+// 携带0向右走的状态
+8, "", N, L, 10
+8, other, N, R
+
+// 携带1向右走的状态
+9, "", N, L, 11
+9, other, N, R
+
+10, "0", "0'", L, 12  // 加'表示此位已固定
+10, "1", "0'", L, 13
+10, "", "0'", R, 14  // 计算逆转二进制字符串已经完成，接下来擦掉特殊记号
+10, other, N, L
+
+11, "0", "1'", L, 12
+11, "1", "1'", L, 13
+11, "", "1'", R, 14  // 计算逆转二进制字符串已经完成，接下来擦掉特殊记号
+11, other, N, L
+
+// 携带0向左走的状态
+12, "", "0'", R, 7
+12, other, N, L
+
+// 携带1向左走的状态
+13, "", "1'", R, 7
+13, other, N, L
+
+14, "", N, L, 15
+14, other, N, R
+
+15, "0'", "0", L
+15, "1'", "1", L
+15, "", N, N, end`;
+
+examples["binary-counting"]["code"]["en"] = `// Binary counting
+
+start, "", N, R
+start, other, N, N, 0
+
+0, "B", "B'", R, 1
+0, "", N, L, 5  // Calculation completed, next erase special marks
+0, other, N, R
+
+1, "", N, R, 2
+1, other, N, R
+
+2, "", "1", L, 3
+2, "0", "1", L, 3
+2, "1", "0", R  // Carry
+
+3, "", N, L, 4
+3, other, N, L
+
+4, "", N, R, 0
+4, other, N, L
+
+5, "B'", "B", L
+5, "", N, R, 6  // Special marks have been erased, next reverse the result. Reversal is needed because the binary number has the least significant bit on the left for calculation convenience
+
+6, "0", N, N, 7
+6, "1", N, N, 7
+6, other, N, R
+
+7, "0", "", R, 8
+7, "1", "", R, 9
+7, "", N, L, 15  // Reversal of binary string completed, next erase special marks
+7, other, N, R
+
+// State moving right carrying 0
+8, "", N, L, 10
+8, other, N, R
+
+// State moving right carrying 1
+9, "", N, L, 11
+9, other, N, R
+
+10, "0", "0'", L, 12  // ' indicates this bit is fixed
+10, "1", "0'", L, 13
+10, "", "0'", R, 14  // Reversal of binary string completed, next erase special marks
+10, other, N, L
+
+11, "0", "1'", L, 12
+11, "1", "1'", L, 13
+11, "", "1'", R, 14  // Reversal of binary string completed, next erase special marks
+11, other, N, L
+
+// State moving left carrying 0
+12, "", "0'", R, 7
+12, other, N, L
+
+// State moving left carrying 1
+13, "", "1'", R, 7
+13, other, N, L
+
+14, "", N, L, 15
+14, other, N, R
+
+15, "0'", "0", L
+15, "1'", "1", L
+15, "", N, N, end`;
+
+examples["binary-counting"]["style"] = {};
+
+examples["binary-counting"]["style"]["zh"] = `B, white, rgb(142,85,55)  // 砖块
+B', white, rgba(142,85,55,0.52)  // 砖块（已数过）`;
+
+examples["binary-counting"]["style"]["en"] = `B, white, rgb(142,85,55)  // Brick
+B', white, rgba(142,85,55,0.52)  // Brick (counted)`;
+
+examples["binary-counting"]["tapes"] = [  // 样例初始纸带
+    ["", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],  // 13个B，正确答案是1101
+    ["", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"]  // 16个B，正确答案是10000
+];
+
+examples["binary-counting"]["embedding"] = {
+  "0": [
+    -0.7681615265037804,
+    -0.03983850733449824
+  ],
+  "1": [
+    -1.0894263585715538,
+    0.33948058848984636
+  ],
+  "2": [
+    -1.5499800377132218,
+    0.3355930077163432
+  ],
+  "3": [
+    -1.6566819412931848,
+    -0.03325629098270456
+  ],
+  "4": [
+    -1.254074736938327,
+    -0.24195285098135255
+  ],
+  "5": [
+    -0.32630594956702985,
+    0.2580572172418985
+  ],
+  "6": [
+    0.16532098256913796,
+    0.45726411587578786
+  ],
+  "7": [
+    0.6724374197542967,
+    0.36597074586783607
+  ],
+  "8": [
+    0.5190272759293288,
+    0.9531316935085736
+  ],
+  "9": [
+    1.2296397250112225,
+    0.008474205608985605
+  ],
+  "10": [
+    0.9733371412501824,
+    0.915005366808394
+  ],
+  "11": [
+    1.4101276902507915,
+    0.35636517115296323
+  ],
+  "12": [
+    1.2610476524416825,
+    0.6932148426884076
+  ],
+  "13": [
+    0.9737479482973489,
+    0.5793389338676962
+  ],
+  "14": [
+    0.9646972983267093,
+    0.24771413434520329
+  ],
+  "15": [
+    0.611271424232641,
+    -0.20968002066149466
+  ],
+  "start": [
+    -0.37314354853449927,
+    -0.3276062764493404
+  ],
+  "self-connection-0": [
+    -0.8012278001750118,
+    -0.30941369488896564
+  ],
+  "self-connection-1": [
+    -1.0584531604231675,
+    0.6491703639070813
+  ],
+  "self-connection-2": [
+    -1.717803290356963,
+    0.5980264543269009
+  ],
+  "self-connection-3": [
+    -1.9230403098894953,
+    -0.189470738052752
+  ],
+  "self-connection-4": [
+    -1.3427993497597392,
+    -0.5490192181195986
+  ],
+  "self-connection-5": [
+    -0.47792240549577447,
+    0.5114996960852614
+  ],
+  "self-connection-6": [
+    0.022697286852661366,
+    0.734668463875273
+  ],
+  "self-connection-7": [
+    0.37953196047302573,
+    0.2321030177775296
+  ],
+  "self-connection-8": [
+    0.39193738606836653,
+    1.248233570603237
+  ],
+  "self-connection-9": [
+    1.448744285714745,
+    -0.2474723651807138
+  ],
+  "self-connection-10": [
+    1.0438304585198326,
+    1.2478888939867918
+  ],
+  "self-connection-11": [
+    1.7411597087572295,
+    0.31367384851154023
+  ],
+  "self-connection-12": [
+    1.5421081912403967,
+    0.8996701763871429
+  ],
+  "self-connection-13": [
+    0.7030258933053509,
+    0.6990099146469617
+  ],
+  "self-connection-14": [
+    0.9202319878271457,
+    -0.026297058262247958
+  ],
+  "self-connection-15": [
+    0.839983104343846,
+    -0.4756634489288382
+  ],
+  "self-connection-16": [
+    0.29158115883568236,
+    -0.2104404471668251
+  ],
+  "end_from_15": [
+    0.41959168443575984,
+    -0.6724353609808762
+  ],
+  "self-connection-17": [
+    -0.26110177624747255,
+    -0.6212011468219711
+  ]
+};
+
+examples["binary-counting"]["recommended-max-steps"] = 3000;  // 推荐的最大步数
---
+++ src/examples/collatz-conjecture.js
@@ -0,0 +1,203 @@
+// 样例代码：二进制除法
+
+if (!examples)
+    var examples = {};
+
+examples["collatz-conjecture"] = {};
+
+examples["collatz-conjecture"]["name"] = {
+    "zh": "考拉兹猜想",
+    "en": "Collatz conjecture"
+}
+
+examples["collatz-conjecture"]["category"] = {
+    "zh": "复杂行为/",
+    "en": "Complex Behavior/"
+};
+
+examples["collatz-conjecture"]["description"] = {
+    "zh": "对于一个正整数，如果它是偶数就除以2，如果它是奇数就乘3再加1。考拉兹猜想假设无论从哪个正整数开始，重复执行上述操作，最终都会演化到1。本程序模拟了二进制数的演化，到1停机。",
+    "en": "For a positive integer, if it is even, divide it by 2; if it is odd, multiply it by 3 and add 1. The Collatz conjecture posits that no matter which positive integer you start with, repeatedly applying the above operations will eventually lead to 1. This program simulates the evolution of a binary number and halts when it reaches 1."
+};
+
+
+examples["collatz-conjecture"]["code"] = {};
+
+examples["collatz-conjecture"]["code"]["zh"] = `// 考拉兹猜想
+
+start, "0", "", R  // 去除开头的0
+start, "1", N, N, 0
+start, other, N, R
+
+// 检查是不是已经迭代到了1
+0, "1", N, R, 1
+
+1, "", N, N, end  // 已经迭代到1，结束
+1, other, N, R, 2
+
+2, "", N, L, 3
+2, other, N, R
+
+3, "0", "", L, 8  // 是偶数，除以2
+3, "1", "1'", L, 4  // 是奇数，开始乘3加1的运算。带'的数字是还没有乘3的位
+
+8, "", N, R, 0
+8, other, N, L
+
+4, "0", "0'", L
+4, "1", "1'", L
+4, "", N, R, 5  // 开始逐位乘3
+
+5, "", N, L, 7  // 所有位都已经乘3了，接下来加1
+5, "0'", "0", R
+5, "1'", "1", L, 6
+5, other, N, R
+
+6, "", "1", R, 5
+6, "0", "1", R, 5
+6, "1", "0", L  // 进位
+
+7, "", "1", N, 0  // 继续进行下一次迭代
+7, "0", "1", N, 0
+7, "1", "0", L  // 进位`;
+
+examples["collatz-conjecture"]["code"]["en"] = `// Collatz conjecture
+
+start, "0", "", R  // Remove leading zeros
+start, "1", N, N, 0
+start, other, N, R
+
+// Check if iteration has reached 1
+0, "1", N, R, 1
+
+1, "", N, N, end  // Reached 1, terminate
+1, other, N, R, 2
+
+2, "", N, L, 3
+2, other, N, R
+
+3, "0", "", L, 8  // Even number, divide by 2
+3, "1", "1'", L, 4  // Odd number, start multiply by 3 and add 1. Digits with ' have not yet been multiplied by 3
+
+8, "", N, R, 0
+8, other, N, L
+
+4, "0", "0'", L
+4, "1", "1'", L
+4, "", N, R, 5  // Start multiplying each digit by 3
+
+5, "", N, L, 7  // All digits have been multiplied by 3, next add 1
+5, "0'", "0", R
+5, "1'", "1", L, 6
+5, other, N, R
+
+6, "", "1", R, 5
+6, "0", "1", R, 5
+6, "1", "0", L  // Carry
+
+7, "", "1", N, 0  // Proceed to next iteration
+7, "0", "1", N, 0
+7, "1", "0", L  // Carry`;
+
+examples["collatz-conjecture"]["style"] = {};
+
+examples["collatz-conjecture"]["style"]["zh"] = undefined;
+
+examples["collatz-conjecture"]["style"]["en"] = `0, white, rgb(79,128,176)
+1, white, rgb(176,79,119)
+0', white, rgba(79,128,176,0.46)
+1', white, rgba(176,79,119,0.46)`;
+
+examples["collatz-conjecture"]["tapes"] = [  // 样例初始纸带
+    ["", "1", "1", "0", "1", "1"],  // 27，经过111步（非图灵机步）演化到1
+    ["", "1", "1"]  // 3，经过6步（非图灵机步）演化到1
+];
+
+examples["collatz-conjecture"]["embedding"] = {
+    "0": [
+      -0.19854447222464688,
+      0.28255747722046065
+    ],
+    "1": [
+      -0.5727085092221835,
+      0.5869933076288096
+    ],
+    "2": [
+      -0.503489230547263,
+      1.0384154844928868
+    ],
+    "3": [
+      -0.05060147821486333,
+      1.1013599820391842
+    ],
+    "4": [
+      0.47690686835204715,
+      1.0936617370498583
+    ],
+    "5": [
+      0.7096581400247676,
+      0.5491863667147815
+    ],
+    "6": [
+      1.0020872012779118,
+      0.7785306391619066
+    ],
+    "7": [
+      0.20660523619456958,
+      0.1887918395022291
+    ],
+    "8": [
+      -0.05647540894429983,
+      0.6882879079926123
+    ],
+    "start": [
+      -0.4483257777184292,
+      -0.09792321399316012
+    ],
+    "end_from_1": [
+      -0.9923232849746907,
+      0.5819740176489462
+    ],
+    "self-connection-0": [
+      -0.7106481786356283,
+      1.2545682455370344
+    ],
+    "self-connection-1": [
+      0.3247524952974819,
+      1.4087101576872356
+    ],
+    "self-connection-2": [
+      0.7414568744802423,
+      1.286705173307561
+    ],
+    "self-connection-3": [
+      0.6616241212575115,
+      0.25065325864700977
+    ],
+    "self-connection-4": [
+      0.9992395689731425,
+      0.33564173896934574
+    ],
+    "self-connection-5": [
+      1.2866824646789556,
+      0.9098847756641467
+    ],
+    "self-connection-6": [
+      0.25752731069275464,
+      -0.1298467408025172
+    ],
+    "self-connection-7": [
+      0.20813498921903198,
+      0.7015560571462389
+    ],
+    "self-connection-8": [
+      -0.7741583540127864,
+      -0.05766605430851992
+    ],
+    "self-connection-9": [
+      -0.3346052008264859,
+      -0.3947898662956654
+    ]
+  };
+
+examples["collatz-conjecture"]["recommended-max-steps"] = 4000;  // 推荐的最大步数
