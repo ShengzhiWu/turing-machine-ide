181ffaf41a8b0851005974f68cbc89c684e32c0c
ShengzhiWu <82275678+ShengzhiWu@users.noreply.github.com>
2026-03-27 01:48:17 +0800

    chore

--- src/render-anim.js
+++ src/render-anim.js
@@ -19,7 +19,7 @@ function menuRenderAnimation() {
 
     // Compute run history
     const renderCode    = parseProgramCode(code_editor_value);
-    const renderHistory = run_turing_machine(renderCode, tape, parseInt(max_steps_input.value), "all", start_position);
+    const renderHistory = run_turing_machine(renderCode, tape, parseInt(max_steps_input.value), "all", start_position, 0);
 
     // Stash history on a module-level variable for use during render
     window._renderHistory = renderHistory;
